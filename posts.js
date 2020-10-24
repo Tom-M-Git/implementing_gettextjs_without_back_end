7f1ed9c13224c47d6d46c4334a6677eef912c81d
Tom-M-Git <astral.taney@gmail.com>
2020-10-24 21:19:17 +0900

    fixes

--- posts.js
+++ posts.js
@@ -1,18 +1,22 @@
 function postsJS(){
 
-    let fetchUrl, fetchXml, xmlParser, parsedXml, xhr;
-    fetchUrl = "components/posts.xml";
+    let xhrUrl, parsedXml, xhr, xhrPromise;
+    xhrUrl = "components/posts.xml";
     xhr = new XMLHttpRequest();
-    if( !(xhr.open("GET", fetchUrl, true)) ){
-        fetchUrl = "../components/posts.xml";
+    if( !(xhr.open("GET", xhrUrl, true)) ){
+        xhrUrl = "../components/posts.xml";
     }
+    xhrPromise = new Promise((resolve, reject)=>{
+        xhr.onload = () => {
+            resolve(xhr.responseXML);
+        };
+        xhr.open("GET", xhrUrl, true);
+        xhr.send();
+    });
 
-    fetchXml = fetch(fetchUrl);
-    fetchXml.then((res)=>{
-        return res.text();
-    }).then((res)=>{
-        xmlParser = new DOMParser();
-        parsedXml = xmlParser.parseFromString(res, "text/xml");
+    
+    xhrPromise.then((res)=>{
+        parsedXml = res;
         loadComponents(parsedXml);
 
     });
